a2df8131372ff01cd1e11b24d291e407bf407006
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-08-26 16:25:21 -0600

    user can complete todos and they dissapear, the delete button still works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,49 @@
 
 
 $(document).ready(function () {
-    $('form').on("submit", function (e) {
+    $('#submit').on("click", function (e) {
         e.preventDefault();
         var toDo = $("#todo").val();
-        $('ul').append('<li>' + toDo + '</li>');
+        $('#todo-list').append('<li id="list-item">' + toDo + '<span class="todo-delete">' + '✗' + '</span>' + '<span class="todo-completed">' + '✓' + '</span>' + '</li>');
 
 
         if (toDo != "") {
-            $('#closingIdButton').show();
-            $('#message').show()
-            window.setTimeout(function () {
-                $('#closingIdButton').hide();
-                $('#message').hide();
-            }, 5000);
+            $('#close').show();
+            $('#message').show();
+            $('#close').delay(5000).fadeOut();
+            $('#message').delay(5000).fadeOut();
         }
-    })
+    });
+
+    $('#close').on("click", function (e) {
+        e.preventDefault();
+        $('#close').hide();
+        $('#message').hide();
+    });
+
+    $('#delete-close').on("click", function (e) {
+        e.preventDefault();
+        $('#delete').hide();
+        $('#delete-close').hide();
+    });
+
+
+
+    $('body').on('click', '.todo-delete', function (e) {
+        e.preventDefault();
+        $('#delete').show();
+        $('#delete-close').show();
+        $(this.parentElement).remove();
+        $('#delete').delay(5000).fadeOut();
+        $('#delete-close').delay(5000).fadeOut();
+    });
+
+    $('body').on('click', '.todo-completed', function (e) {
+        e.preventDefault();
+        var row = $(this.parentElement);
+        $(this.parentElement).remove();
+        $('#todo-completed').append(row);
+    });
+
+//    This is not working, the 'close' button is activating the 'submit value for the form.
 });
